b5299640e5058ab33c1a949eccc46dda9beea9bd
Mariana Campos <marianacampos@MacBook-Pro-de-actdigital.local>
2021-02-28 20:01:03 -0300

    finalizando projeto

---
+++ src/controllers/AnswerController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { AppError } from "../errors/AppError";
+import { SurveyUser } from "../models/SurveyUser";
+import { SurveysRepository } from "../repositories/SurveysRepository";
+import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
+
+class AnswerController {
+
+  async execute(request: Request, response: Response) {
+    const { value } = request.params;
+    const { u } = request.query;
+
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
+
+    const surveyUser = await surveysUsersRepository.findOne({
+      id: String(u),
+    })
+
+    if (!surveyUser) {
+      throw new AppError("Survey User does not exists")
+      //return response.status(400).json({
+      //  error: "Survey User does not exists"})
+    }
+    
+    surveyUser.value = Number(value);
+
+    await surveysUsersRepository.save(surveyUser);
+
+    return response.json(surveyUser);
+  }
+}
+  
+
+export { AnswerController }
